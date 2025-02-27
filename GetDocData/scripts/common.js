22e41c1a51d6dd460cf9b103ae9c718bda5b785a
Santhoshpantala <115636227+Santhoshpantala@users.noreply.github.com>
2025-02-27 20:07:52 +0530

    Add files via upload

--- GetDocData/scripts/common.js
+++ GetDocData/scripts/common.js
@@ -1,9 +1,11 @@
 
 define(["DS/WAFData/WAFData", "DS/i3DXCompassServices/i3DXCompassServices", "DS/PlatformAPI/PlatformAPI","DS/GetDocData/scripts/common"],function(WAFData, i3DXCompassServices, PlatformAPI) {
     "use strict";
-    var appConfigs ="";
+    
     // Example function you want to export
     function getServiceURL() {
+        var appConfigs ="";
+        var SpaceURl ="";
         console.log("Calling for Service URL");
         //var spaceURL = PlatformAPI.getApplicationConfiguration("app.urls.myapps");
         appConfigs = PlatformAPI.getAllApplicationConfigurations(); 
@@ -12,6 +14,16 @@ define(["DS/WAFData/WAFData", "DS/i3DXCompassServices/i3DXCompassServices", "DS/
         //console.log("dataset URL:::::::::", dataSetSpaceURL);
         //var url = dataSetSpaceURL + "?searchStr=" + docTitle;
         //console.log("URL ::::::::::::", url);
+        for(const element of appConfigs)
+        {
+            if(element['propertyKey'] == 'app.urls.myapps')
+            {
+                SpaceURl = element['propertyValue'];
+                console.log("Space URl :::::::::::::;",SpaceURl);
+
+            }
+
+        }
     }
 
     // Export the function to be used in other files
